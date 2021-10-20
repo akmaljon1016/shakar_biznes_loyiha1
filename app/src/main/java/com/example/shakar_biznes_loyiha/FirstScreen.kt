41150427c7c9f97d0bb4fd9802d8013283a4d6c5
Akmaljon1016 <akmaljonsotvoldiev1998@gmail.com>
2021-10-20 15:54:21 -0700

    remove First Screen Comment

--- app/src/main/java/com/example/shakar_biznes_loyiha/FirstScreen.kt
+++ app/src/main/java/com/example/shakar_biznes_loyiha/FirstScreen.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
 import com.example.shakar_biznes_loyiha.utils.UserPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -12,28 +14,28 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class FirstScreen : AppCompatActivity(R.layout.activity_main) {
-    var isUserLogged: Boolean = true
+    var isUserLogged: Boolean = false
 
     private val userPreferences by lazy { UserPreferences(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        userPreferences.isUserLOgged.asLiveData().observe(
-//            this,
-//            Observer {
-//                if (it == null || it == false) {
-//                    isUserLogged = false
-//                }
-//                if (it == true) {
-//                    isUserLogged = true
-//                }
-//            },
-//        )
+        userPreferences.isUserLOgged.asLiveData().observe(
+            this,
+            Observer {
+                if (it == null || it == false) {
+                    isUserLogged = false
+                }
+                if (it == true) {
+                    isUserLogged = true
+                }
+            },
+        )
 
         CoroutineScope(Dispatchers.Main).launch {
             val animation =
                 AnimationUtils.loadAnimation(applicationContext, R.anim.splash_animation)
             splashTextView.startAnimation(animation)
-            delay(0)
+            delay(2000)
             if (isUserLogged) {
                 startActivity(Intent(this@FirstScreen, HomeActivity::class.java))
                 finish()
